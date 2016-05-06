50b3bed3963cd8e66384fed26ecaaa6a900f1ac5
albertraza <arbert.jr@gmail.com>
2016-05-06 10:20:47 -0400

    Create contrato methods and forms

---
+++ Cely Sistema/Cely Sistema/frmReporte.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Reporting.WinForms;
+
+namespace Cely_Sistema
+{
+    public partial class frmReporte : Form
+    {
+        public string Matricula { get; set; }
+        public string ModoPago { get; set; }
+        public string VIP { get; set; }
+
+        public frmReporte()
+        {
+            InitializeComponent();
+        }
+
+        private void frmReporte_Load(object sender, EventArgs e)
+        {
+            LoadReport();
+            this.reportViewer1.RefreshReport();
+        }
+        private void LoadReport()
+        {
+            if(VIP == "NO" || VIP == "No")
+            {
+                // for non VIP Students.
+                if(ModoPago == "Mensual")
+                {
+                    // when payment is monthly
+                    DataTable dtPagoMensual = reportes.contratoEstudianteMensual(Matricula);
+                    reportViewer1.Reset();
+                    reportViewer1.LocalReport.ReportPath = "Contrato.rdlc";
+                    ReportDataSource ds = new ReportDataSource("Contrato", dtPagoMensual);
+                    reportViewer1.LocalReport.DataSources.Add(ds);
+                    reportViewer1.Refresh();
+                }
+                else
+                {
+                    // when payment is weekly
+                    DataTable dtPagoSemanal = reportes.contratoEstudianteSemanal(Matricula);
+                    reportViewer1.Reset();
+                    reportViewer1.LocalReport.ReportPath = "Contrato.rdlc";
+                    ReportDataSource ds = new ReportDataSource("Contrato", dtPagoSemanal);
+                    reportViewer1.LocalReport.DataSources.Add(ds);
+                    reportViewer1.Refresh();
+                }
+            }
+            else
+            {
+                // for VIP Students.
+                if(ModoPago == "Mensual")
+                {
+                    // when payment is monthly
+                    DataTable dtPagoVIPMensual = reportes.contratoEstudianteVIPMensual(Matricula);
+                    reportViewer1.Reset();
+                    reportViewer1.LocalReport.ReportPath = "Contrato.rdlc";
+                    ReportDataSource ds = new ReportDataSource("Contrato", dtPagoVIPMensual);
+                    reportViewer1.LocalReport.DataSources.Add(ds);
+                    reportViewer1.Refresh();
+                }
+                else
+                {
+                    // when payment is weekly
+                    DataTable dtPagoVIPSemanal = reportes.contratoEstudianteVIPSemanal(Matricula);
+                    reportViewer1.Reset();
+                    reportViewer1.LocalReport.ReportPath = "Contrato.rdlc";
+                    ReportDataSource ds = new ReportDataSource("Contrato", dtPagoVIPSemanal);
+                    reportViewer1.LocalReport.DataSources.Add(ds);
+                    reportViewer1.Refresh();
+                }
+            }
+        }
+    }
+}
---
+++ Cely Sistema/Cely Sistema/reportes.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Cely_Sistema
+{
+    public class reportes
+    {
+        // report for VIP students where their payment is monthly
+        public static DataTable contratoEstudianteVIPMensual(string Matricula)
+        {
+            DataTable r = new DataTable();
+            using(SqlConnection con = DBcomun.ObetenerConexion())
+            {
+                SqlDataAdapter da = new SqlDataAdapter(string.Format("Select Estudiantes.ID as Matricula, Nombre, Apellido, FechaN as FechaNacimiento, Edad, Telefono, Celular, Email, Sector, Direccion, Ocupacion, NivelA as NivelAcademico, DominioIdiomaIngles as DominioIdiomaIngles, Pago_VIP as PagoMensualSemanal, Mora_Mensual as Mora, VIP from Estudiantes Inner join CantidadPagoVIP on CantidadPagoVIP.ID = 1 where VIP = 'Si' and Modo_Pago = 'Mensual' and Estudiantes.ID = '{0}'", Matricula), con);
+                da.Fill(r);
+                con.Close();
+            }
+            return r;
+        }
+        // report for VIP students where their payment is weekly
+        public static DataTable contratoEstudianteVIPSemanal(string Matricula)
+        {
+            DataTable r = new DataTable();
+            using (SqlConnection con = DBcomun.ObetenerConexion())
+            {
+                SqlDataAdapter da = new SqlDataAdapter(string.Format("Select Estudiantes.ID as Matricula, Nombre, Apellido, FechaN as FechaNacimiento, Edad, Telefono, Celular, Email, Sector, Direccion, Ocupacion, NivelA as NivelAcademico, DominioIdiomaIngles as DominioIdiomaIngles, Pago_Semanal as PagoMensualSemanal, Mora_Semanal as Mora, VIP from Estudiantes Inner join CantidadPagoVIP on CantidadPagoVIP.ID = 1 where VIP = 'Si' and Modo_Pago = 'Semanal' and Estudiantes.ID = '{0}'", Matricula), con);
+                da.Fill(r);
+                con.Close();
+            }
+            return r;
+        }
+        // report for non VIP students where their payment is weekly
+        public static DataTable contratoEstudianteSemanal(string Matricula)
+        {
+            DataTable r = new DataTable();
+            using (SqlConnection con = DBcomun.ObetenerConexion())
+            {
+                SqlDataAdapter da = new SqlDataAdapter(string.Format("Select Estudiantes.ID as Matricula, Nombre, Apellido, FechaN as FechaNacimiento, Edad, Telefono, Celular, Email, Sector, Direccion, Ocupacion, NivelA as NivelAcademico, DominioIdiomaIngles as DominioIdiomaIngles, Pago_Semanal as PagoMensualSemanal, Mora_Semanal as Mora, VIP from Estudiantes Inner join CantidadPago on CantidadPago.ID = 1 inner join Mora on Mora.ID = 1 where VIP = 'NO' and Modo_Pago = 'Semanal' and Estudiantes.ID = '{0}'", Matricula), con);
+                da.Fill(r);
+                con.Close();
+            }
+            return r;
+        }
+        // report for non VIP students where their payment is monthly
+        public static DataTable contratoEstudianteMensual(string Matricula)
+        {
+            DataTable r = new DataTable();
+            using (SqlConnection con = DBcomun.ObetenerConexion())
+            {
+                SqlDataAdapter da = new SqlDataAdapter(string.Format("Select Estudiantes.ID as Matricula, Nombre, Apellido, FechaN as FechaNacimiento, Edad, Telefono, Celular, Email, Sector, Direccion, Ocupacion, NivelA as NivelAcademico, DominioIdiomaIngles as DominioIdiomaIngles, Pago_Mensual as PagoMensualSemanal, Mora_Mensual as Mora, VIP from Estudiantes Inner join CantidadPago on CantidadPago.ID = 1 inner join Mora on Mora.ID = 1 where VIP = 'NO' and Modo_Pago = 'Mensual' and Estudiantes.ID = '{0}'", Matricula), con);
+                da.Fill(r);
+                con.Close();
+            }
+            return r;
+        }
+    }
+}
